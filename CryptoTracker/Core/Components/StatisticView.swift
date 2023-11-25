eefbb7c3fc79b6d0b7fa36e1dc852e7a75dbd34a
Valery Zazulin <zazulinvalerii@gmail.com>
2023-11-26 00:17:16 +0100

    Created a StatisticModel and HomeStatViews to add them on the HomeScreen above the searchBar. Added an animation so it moves aligned with the coin list when we tap an arrow button to look user's portfolio.

---
+++ CryptoTracker/Core/Components/StatisticView.swift
@@ -0,0 +1,45 @@
+//
+//  StatisticView.swift
+//  CryptoTracker
+//
+//  Created by Валерий Зазулин on 25.11.2023.
+//
+
+import SwiftUI
+
+struct StatisticView: View {
+    
+    let stat: StatisticModel
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4) {
+            Text(stat.title)
+                .font(.caption)
+                .foregroundStyle(Color.theme.secondaryText)
+            
+            Text(stat.value)
+                .font(.headline)
+                .foregroundStyle(Color.theme.accent)
+            
+            HStack(spacing: 4) {
+                Image(systemName: "triangle.fill")
+                    .font(.caption2)
+                    .rotationEffect(
+                        Angle(
+                            degrees: (stat.percentageChange ?? 0) >= 0 ? 0 : 180))
+                
+                Text(stat.percentageChange?.asPercentString() ?? "")
+                    .font(.caption)
+                .bold()
+            }
+            .foregroundStyle((stat.percentageChange ?? 0) >= 0 ? Color.theme.green : Color.theme.red)
+            .opacity(stat.percentageChange == nil ? 0 : 1)
+        }
+    }
+}
+
+struct StatisticView_Previews: PreviewProvider {
+    static var previews: some View {
+        StatisticView(stat: dev.stat3)
+    }
+}
