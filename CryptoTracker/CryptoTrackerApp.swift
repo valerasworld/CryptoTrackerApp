4e83811a93fb8e8d6ac569757eed19d72e0d15d4
Valery Zazulin <zazulinvalerii@gmail.com>
2023-11-29 18:48:43 +0100

    Changed the color of NavigationBar Texts to our accent color.

--- CryptoTracker/CryptoTrackerApp.swift
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -12,6 +12,12 @@ struct CryptoTrackerApp: App {
     
     @StateObject private var viewModel = HomeViewModel()
     
+    init() {
+        UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
+
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
