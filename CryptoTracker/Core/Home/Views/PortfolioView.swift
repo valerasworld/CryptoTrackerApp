36136c6708400ec2e9dc98402f73a98c39fd3a58
Valery Zazulin <zazulinvalerii@gmail.com>
2023-12-01 01:24:53 +0100

    CoreData is implemented to the HomeViewModel via PortfolioDataService instance. Now portfolio data persists and updates when we change them in the PortfolioView.

--- CryptoTracker/Core/Home/Views/PortfolioView.swift
+++ CryptoTracker/Core/Home/Views/PortfolioView.swift
@@ -57,14 +57,14 @@ struct PortfolioView_Previews: PreviewProvider {
 extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false , content: {
-            LazyHStack(spacing: 10) {
-                ForEach(viewModel.allCoins) { coin in
+            HStack(spacing: 10) {
+                ForEach(viewModel.searchText.isEmpty ? viewModel.portfolioCoins : viewModel.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(
@@ -77,6 +77,17 @@ extension PortfolioView {
             .frame(height: 120)
             .padding(.leading)
         })
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        
+        if let portfolioCoin = viewModel.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = "\(amount)"
+        } else {
+            quantityText = ""
+        }
     }
     
     private var portfolioInputSection: some View {
@@ -131,10 +142,13 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else { return }
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantityText.replacingOccurrences(of: ",", with: "."))
+        else { return }
         
         // save to portfolio
-        
+        viewModel.updatePortfolio(coin: coin, amount: amount)
         
         // show checkmark
         withAnimation(.easeIn) {
